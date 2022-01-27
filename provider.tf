8ede675511c935c02ec263647eace3ebf54480ae
Henry McConville <henry.mcconville@infinityworks.com>
2022-01-27 16:20:54 +0000

    align azure test with aws one

--- provider.tf
+++ provider.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.90.0"
+      version = "=2.93.1"
     }
   }
+  required_version = ">= 1.0.0"
 }
 
 
 provider "azurerm" {
   features {}
-
   skip_provider_registration = true
 }
